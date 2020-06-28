90286efbd922af26d0585532be671d6ea2d594fd
Kashyap <deepak.kashyap.ext@nokia.com>
2020-06-28 12:19:40 +0530

    commit change file

---
+++ springboot-rabbitmq-consumer/src/main/java/com/deep/ms/springbootrabbitmqconsumer/service/RabbitMQListner.java
@@ -0,0 +1,16 @@
+package com.deep.ms.springbootrabbitmqconsumer.service;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQListner implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		System.out.println("Consuming Message - " + new String(message.getBody()));
+
+	}
+
+}
